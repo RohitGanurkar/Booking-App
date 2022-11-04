baf185f37badcbc8effc7eaeaba2d1019ad21cbf
Rahul <rketu786@gmail.com>
2022-11-05 01:07:35 +0530

    Half Api is Done

---
+++ server/routes/rooms.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  updateRoom,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+export default router;
